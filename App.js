1e95cc7b84f2dae600d78472d2a221879526f46d
L-0-W <typeint@proton.me>
2026-03-09 22:01:37 -0300

    'enviando atividade: exercicio-01-lista-produtos'

--- App.js
+++ App.js
@@ -26,10 +26,4 @@ const styles = StyleSheet.create({
     backgroundColor: '#ecf0f1',
     padding: 8,
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
 });
